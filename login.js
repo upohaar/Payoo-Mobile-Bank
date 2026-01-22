bb12c9c8d743eff1cabef01ad79e76572f75a95a
upohaar <sumaiyanobiupohaar@gmail.com>
2026-01-22 22:17:12 +0600

    dashboard file added

---
+++ login.js
@@ -0,0 +1,17 @@
+document.getElementById('btn-login').addEventListener('click', function() {
+   const emailField = document.getElementById('email');
+    const passwordField = document.getElementById('password');
+    const message = document.getElementById('message');
+    const email = emailField.value;
+    const password = passwordField.value;
+  if (email === 'admin@test.com' && password === '1234') {
+    alert('লগইন সফল হয়েছে!');
+        window.location.href = 'dashboard.html';
+      } 
+    else {
+        
+        message.innerText = 'ভুল ইমেইল অথবা পাসওয়ার্ড! আবার চেষ্টা করুন।';
+        emailField.style.borderColor = 'red';
+        passwordField.style.borderColor = 'red';
+    }
+});
